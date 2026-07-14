216a541eb49c8d6e51fbdd30902a69ce23409d99
Lőrinc Kóródi <korodilorinc@gmail.com>
2026-07-14 09:33:08 +0200

    Fixed problems with Docker

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -11,9 +11,8 @@ use handlers::{get_files, get_peaks};
 
 #[tokio::main]
 async fn main() {
-    let state = AppState{
-        db_path: Arc::new("../results/peaks.db".to_string()),
-    };
+    let db_path = std::env::var("DATABASE_PATH").unwrap_or_else(|_| "../results/peaks.db".to_string());
+    let state = AppState { db_path: Arc::new(db_path) };
 
     let router = Router::new()
     .route("/list",get(get_files))
